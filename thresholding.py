40ad202e3b3c150729525c4bb7bf02de8877c6f0
mohadeseh <m.ghafoori78@gmail.com>
2022-04-26 13:20:13 +0430

    add some comments

--- thresholding.py
+++ thresholding.py
@@ -12,5 +12,8 @@ threshold,inv_threshed_img=cv.threshold(gray_img,127,255,cv.THRESH_BINARY_INV)
 cv.imshow("inverse thresholding",inv_threshed_img)
 #adaptive thresholding 
 adaptive_threshed_img=cv.adaptiveThreshold(gray_img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
+#cv.ADAPTIVE_THRESH_MEAN_C: The threshold value is the mean of the neighbourhood area minus the constant C.
+#cv.ADAPTIVE_THRESH_GAUSSIAN_C: The threshold value is a gaussian-weighted sum of the neighbourhood values minus the constant C
+#The blockSize determines the size of the neighbourhood area and C is a constant that is subtracted from the mean or weighted sum of the neighbourhood pixels.
 cv.imshow("adaptive thresholding",adaptive_threshed_img)
 cv.waitKey(0)
