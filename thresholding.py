c2a98f064a7c8d7114923929843dd2c767daf082
mohadeseh <m.ghafoori78@gmail.com>
2022-04-26 13:16:18 +0430

    adaptive thresholding

--- thresholding.py
+++ thresholding.py
@@ -10,4 +10,7 @@ cv.imshow("threshed image", threshed_img)
 #inverse thresholding
 threshold,inv_threshed_img=cv.threshold(gray_img,127,255,cv.THRESH_BINARY_INV)
 cv.imshow("inverse thresholding",inv_threshed_img)
+#adaptive thresholding 
+adaptive_threshed_img=cv.adaptiveThreshold(gray_img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
+cv.imshow("adaptive thresholding",adaptive_threshed_img)
 cv.waitKey(0)
